34946575f3f9a06b2aaa59d57fb54154a9f6df90
Andrey N Laktyushin <andrey@laktyushin.com>
2022-06-16 22:29:57 +0300

    1. Запуск нити. Thread#start() [#1016 #313211]

--- src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -11,16 +11,13 @@ public class ConcurrentOutput {
                 () -> System.out.println(Thread.currentThread().getName())
         );
         second.start();
-        System.out.println(Thread.currentThread().getName());
         Thread third = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
         third.start();
-        System.out.println(Thread.currentThread().getName());
         Thread noName = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
         noName.start();
-        System.out.println(Thread.currentThread().getName());
     }
 }
