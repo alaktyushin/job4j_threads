38de97faa309ce52ea3073aca236ee21f9e548d9
Andrey N Laktyushin <andrey@laktyushin.com>
2022-06-17 12:48:02 +0300

    1.1. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,27 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {}
+        );
+        Thread second = new Thread(
+                () -> {}
+        );
+        System.out.println(first.getName() + " " + first.getState());
+        System.out.println(second.getName() + " " + second.getState());
+        first.start();
+        second.start();
+        do {
+            System.out.println(first.getName() + " " + first.getState());
+            System.out.println(second.getName() + " " + second.getState());
+        } while ((first.getState() != Thread.State.TERMINATED)
+                && (second.getState() != Thread.State.TERMINATED));
+        System.out.println(Thread.currentThread().getName()
+                + " thread says: "
+                + first.getName()
+                + " and "
+                + second.getName()
+                + " are terminated.");
+    }
+}
