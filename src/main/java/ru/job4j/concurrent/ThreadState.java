abeafaa79353534ba2a1fb03b7453e471d9f96fd
Andrey N Laktyushin <andrey@laktyushin.com>
2022-06-22 13:19:00 +0300

    1.1. Состояние нити. [#229175 #313395]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -3,20 +3,20 @@ package ru.job4j.concurrent;
 public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(
-                () -> {}
+                () -> System.out.println("New object \"Thread first\" created")
         );
         Thread second = new Thread(
-                () -> {}
+                () -> System.out.println("New object \"Thread second\" created")
         );
         System.out.println(first.getName() + " " + first.getState());
         System.out.println(second.getName() + " " + second.getState());
         first.start();
         second.start();
-        do {
+        while ((first.getState() != Thread.State.TERMINATED)
+                || (second.getState() != Thread.State.TERMINATED)) {
             System.out.println(first.getName() + " " + first.getState());
             System.out.println(second.getName() + " " + second.getState());
-        } while ((first.getState() != Thread.State.TERMINATED)
-                && (second.getState() != Thread.State.TERMINATED));
+        }
         System.out.println(Thread.currentThread().getName()
                 + " thread says: "
                 + first.getName()
