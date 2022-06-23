7eaf28653e396019d64f91ffce320038048f4b88
Andrey N Laktyushin <andrey@laktyushin.com>
2022-06-23 09:39:14 +0300

    3. Immutable объекты [#267918]

---
+++ src/main/java/ru/job4j/linked/Node.java
@@ -0,0 +1,29 @@
+package ru.job4j.linked;
+
+public class Node<T> {
+    private final Node<T> next;
+    private final T value;
+
+    public Node(final Node<T> next, final T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        throw new IllegalStateException(
+                String.format("Can't change immutable object state (next): %s", next));
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        throw new IllegalStateException(
+                String.format("Can't change immutable object state (value): %s", value));
+    }
+}
