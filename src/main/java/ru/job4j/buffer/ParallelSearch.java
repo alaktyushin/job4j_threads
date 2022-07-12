b897b483e7a2bc3b713a0a91434f93cb1abaf667
Andrey N Laktyushin <andrey@laktyushin.com>
2022-07-12 14:04:07 +0300

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,37 @@
+package ru.job4j.buffer;
+
+import ru.job4j.monitor.SimpleBlockingQueue;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        final int QUEUE_SIZE = 3;
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(QUEUE_SIZE);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (true) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            System.out.println("Consumer stopped by exception: " + e);
+                            break;
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != QUEUE_SIZE; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    consumer.interrupt();
+                }
+        ).start();
+    }
+}
